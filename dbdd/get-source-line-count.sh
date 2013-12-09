fc5c27b4c35a36c1e0064ca347e1c6c01fc191b2
Daniel <arckuno@gmail.com>
2013-12-09 09:39:18 -0700

    Added SQL to the source line count report.

--- dbdd/get-source-line-count.sh
+++ dbdd/get-source-line-count.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-pushd prj.link > /dev/null
-wc -l `find . -iname "*.java"`
+pushd .. > /dev/null
+FILES=$(sort < <(find java -iname "*.java"; echo ddl/Tables.sql; echo test-data/Insertion.sql; echo ddl/conv.sh) )
+wc -l $FILES
 popd > /dev/null
